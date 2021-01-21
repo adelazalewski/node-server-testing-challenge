29deaeb80c6a66b0aa7d30328e0e2a8ce2471675
adelazalewski <paliuadela@yahoo.com>
2021-01-21 15:46:25 -0500

    finished setting up the project

---
+++ data/seeds/01-todos.js
@@ -0,0 +1,11 @@
+
+exports.seed =async function(knex) {
+ await knex("todos").truncate()
+ await knex("todos").insert([
+   {todo_item: "clean house", created_at: "2018-01-05"},
+   {todo_item: "clean fridge", created_at: "2018-01-05"},
+   {todo_item: "clean bathroom", created_at: "2018-01-05"},
+   {todo_item: "finish project", created_at: "2018-01-05"},
+   {todo_item: "meal plan", created_at: "2018-01-05"}
+ ])
+};
